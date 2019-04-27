ebbab41ecd74e6be5b5e35d829b122adc8ec5fa0
Swastik <sinpman@outlook.com>
2019-04-27 19:20:13 +0000

    Going barebones

--- application.py
+++ application.py
@@ -1,15 +1,8 @@
-import os
-import datetime
-
 from flask import Flask, redirect, render_template, request, url_for, session
-from flask_session import Session
-from flask_bcrypt import generate_password_hash, check_password_hash
-from sqlalchemy import create_engine, exc
-from sqlalchemy.orm import scoped_session, sessionmaker
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
